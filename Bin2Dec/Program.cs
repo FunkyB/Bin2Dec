17776ca3793786c648eafd2d124d41b9b7c85141
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2021-06-17 09:38:13 +0300

    Initial commit

--- Bin2Dec/Program.cs
+++ Bin2Dec/Program.cs
@@ -6,7 +6,18 @@ namespace Bin2Dec
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter up to 8 binary digits: ");
+
+            var number = Console.ReadLine();
+
+            bool TryParse(string? s, out byte result);
+
+            Console.WriteLine(number);
+
+            //if (number Contains 0)
+            //{
+            //    Console.WriteLine($"Число {num1} больше числа {num2}");
+            //}
         }
     }
 }
