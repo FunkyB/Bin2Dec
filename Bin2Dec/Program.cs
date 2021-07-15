ba0e3d805c23cdf3d22170fdb40ccc0b7cbb433d
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2021-07-15 11:44:43 +0300

    Refactor code

--- Bin2Dec/Program.cs
+++ Bin2Dec/Program.cs
@@ -7,21 +7,23 @@ namespace Bin2Dec
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter up to 8 binary digits: ");
-            string input = Console.ReadLine();
-            bool validInt = int.TryParse(input, out int inputValue);
-            bool validInput = input.Length < 9 && input.All(ch => ch == '0' || ch == '1') && validInt;
-            while (!validInput)
+            string again = null;
+            do
             {
-                Console.WriteLine("Invalid Input. Please try again");
                 Console.Write("Enter up to 8 binary digits: ");
-                input = Console.ReadLine();
-                validInt = int.TryParse(input, out inputValue);
-                validInput = input.Length < 9 && input.All(ch => ch == '0' || ch == '1') && validInt;
-            }
-            int decNum = Convert.ToInt32(inputValue.ToString(), 2);
+                string input = Console.ReadLine();
 
-            Console.WriteLine(decNum);
+                if (input.Length < 9 && input.All(ch => ch == '0' || ch == '1'))
+                {
+                    int decNum = Convert.ToInt32(input.ToString(), 2);
+                    Console.WriteLine($"Decimal value of bin {input} = {decNum}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input. Please try again");
+                }
+            }
+            while (again == null);
         }
     }
 }
